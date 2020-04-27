9200ff1174b9932640e896f73a7cfb794e17599f
Matthew Cain <yooksi@zoho.com>
2020-04-28 00:32:14 +0200

    Define modid in mod entry point

--- src/main/java/io/puppetzmedia/ttweaks/TwistedTweaks.java
+++ src/main/java/io/puppetzmedia/ttweaks/TwistedTweaks.java
@@ -1,5 +1,6 @@
 package io.puppetzmedia.ttweaks;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -10,6 +11,7 @@ import org.apache.logging.log4j.Logger;
 @Mod("twistedtweaks")
 public class TwistedTweaks {
 
+    public static final String MODID = "twistedtweaks";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public TwistedTweaks() {
@@ -22,5 +24,11 @@ public class TwistedTweaks {
 
     private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("Pre-initialization phase...");
+    }
+    /**
+     * @return {@code ResourceLocation} pointing to provided path with {@link #MODID} as namespace
+     */
+    public static ResourceLocation location(String path) {
+        return new ResourceLocation(MODID, path);
     }
 }
