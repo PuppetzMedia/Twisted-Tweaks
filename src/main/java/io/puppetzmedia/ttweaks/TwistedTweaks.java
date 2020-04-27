c044fed29471cb2e623a4fb585de3756199cbccf
Matthew Cain <yooksi@zoho.com>
2020-04-28 00:33:46 +0200

    Start listening for FMLClientSetupEvent

--- src/main/java/io/puppetzmedia/ttweaks/TwistedTweaks.java
+++ src/main/java/io/puppetzmedia/ttweaks/TwistedTweaks.java
@@ -3,6 +3,7 @@ package io.puppetzmedia.ttweaks;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -18,7 +19,10 @@ public class TwistedTweaks {
         // Register the setup method for modloading
         // Initialize mod logger
         TTLogger.init(LogManager.getLogger());
+
+        // Register the setup methods for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
 
         // Register ourselves for game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -27,6 +31,11 @@ public class TwistedTweaks {
     private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("Pre-initialization phase...");
     }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        LOGGER.info("Post-initialization phase...");
+    }
+
     /**
      * @return {@code ResourceLocation} pointing to provided path with {@link #MODID} as namespace
      */
