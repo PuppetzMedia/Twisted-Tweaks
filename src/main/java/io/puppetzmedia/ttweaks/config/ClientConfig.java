50fdfcc0766dc6a7fafc4b2a3db5fcf2d27cc9c7
Matthew Cain <yooksi@zoho.com>
2020-04-28 04:16:44 +0200

    Add mod config base classes

---
+++ src/main/java/io/puppetzmedia/ttweaks/config/ClientConfig.java
@@ -0,0 +1,14 @@
+package io.puppetzmedia.ttweaks.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ClientConfig {
+
+	public ClientConfig(ForgeConfigSpec.Builder builder) {
+
+		// Enter our configurations here
+
+		// Finish building configurations
+		builder.build();
+	}
+}
---
+++ src/main/java/io/puppetzmedia/ttweaks/config/TwistedTweaksConfig.java
@@ -0,0 +1,35 @@
+package io.puppetzmedia.ttweaks.config;
+
+import io.puppetzmedia.ttweaks.TwistedTweaks;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import org.apache.commons.lang3.tuple.Pair;
+
+@Mod.EventBusSubscriber(modid = TwistedTweaks.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class TwistedTweaksConfig {
+
+	public static final ClientConfig CLIENT;
+	public static final ForgeConfigSpec CLIENT_SPEC;
+
+	static {
+		final Pair<ClientConfig, ForgeConfigSpec> specPair =
+				new ForgeConfigSpec.Builder().configure(ClientConfig::new);
+
+		CLIENT_SPEC = specPair.getRight();
+		CLIENT = specPair.getLeft();
+	}
+
+	public static void bakeConfig() {
+		// Bake our configs here
+	}
+
+	@SubscribeEvent
+	public static void onModConfigEvent(final ModConfig.ModConfigEvent configEvent) {
+
+		if (configEvent.getConfig().getSpec() == CLIENT_SPEC) {
+			bakeConfig();
+		}
+	}
+}
