5178ff7a043685077c3d8feb43c28ba79f54f4c7
Matthew Cain <yooksi@zoho.com>
2020-04-28 04:30:31 +0200

    Register mod config from main class

--- src/main/java/io/puppetzmedia/ttweaks/TwistedTweaks.java
+++ src/main/java/io/puppetzmedia/ttweaks/TwistedTweaks.java
@@ -1,8 +1,11 @@
 package io.puppetzmedia.ttweaks;
 
+import io.puppetzmedia.ttweaks.config.TwistedTweaksConfig;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -16,13 +19,15 @@ public class TwistedTweaks {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public TwistedTweaks() {
-        // Register the setup method for modloading
         // Initialize mod logger
         TTLogger.init(LogManager.getLogger());
 
         // Register the setup methods for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+
+        // Register Mod configuration
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, TwistedTweaksConfig.CLIENT_SPEC);
 
         // Register ourselves for game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
