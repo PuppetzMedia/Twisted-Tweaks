50bb41fd027c0941662ae92092e4ef1dda1e1559
Matthew Cain <yooksi@zoho.com>
2020-04-28 03:54:45 +0200

    Disable torch light and particles

--- src/main/java/io/puppetzmedia/ttweaks/block/TorchUnlitBlock.java
+++ src/main/java/io/puppetzmedia/ttweaks/block/TorchUnlitBlock.java
@@ -1,12 +1,25 @@
 package io.puppetzmedia.ttweaks.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.TorchBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Random;
 
 public class TorchUnlitBlock extends TorchBlock {
 
 	public TorchUnlitBlock() {
-		super(Block.Properties.from(Blocks.TORCH));
+		super(Block.Properties.from(Blocks.TORCH).lightValue(0));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		// Disable spawning flame and smoke particles
 	}
 }
--- src/main/java/io/puppetzmedia/ttweaks/block/WallTorchUnlitBlock.java
+++ src/main/java/io/puppetzmedia/ttweaks/block/WallTorchUnlitBlock.java
@@ -1,12 +1,25 @@
 package io.puppetzmedia.ttweaks.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.WallTorchBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Random;
 
 public class WallTorchUnlitBlock extends WallTorchBlock {
 
 	public WallTorchUnlitBlock() {
-		super(Block.Properties.from(Blocks.WALL_TORCH));
+		super(Block.Properties.from(Blocks.WALL_TORCH).lightValue(0));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		// Disable spawning flame and smoke particles
 	}
 }
