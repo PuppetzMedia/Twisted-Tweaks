612f7e60b2793518a05966dec6d36308b80c4d4c
Matthew Cain <yooksi@zoho.com>
2020-05-01 00:19:24 +0200

    Port glowstone torch class code

--- src/main/java/io/puppetzmedia/ttweaks/block/GlowstoneWallTorch.java
+++ src/main/java/io/puppetzmedia/ttweaks/block/GlowstoneWallTorch.java
@@ -3,9 +3,10 @@ package io.puppetzmedia.ttweaks.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.TorchBlock;
-import net.minecraft.particles.IParticleData;
+import net.minecraft.block.WallTorchBlock;
 import net.minecraft.particles.RedstoneParticleData;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
@@ -13,10 +14,13 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 import java.util.Random;
 
-public class GlowstoneTorch extends TorchBlock {
+public class GlowstoneWallTorch extends WallTorchBlock {
 
-	public GlowstoneTorch() {
-		super(Block.Properties.from(Blocks.TORCH).lightValue(1));
+	public GlowstoneWallTorch(int lightValue) {
+		super(Block.Properties.from(Blocks.WALL_TORCH).lightValue(lightValue));
+	}
+	public GlowstoneWallTorch() {
+		super(Block.Properties.from(Blocks.WALL_TORCH));
 	}
 
 	@Override
@@ -30,7 +34,12 @@ public class GlowstoneTorch extends TorchBlock {
 		double d1 = (double) pos.getY() + 0.7D;
 		double d2 = (double) pos.getZ() + 0.5D;
 
-		IParticleData data = RedstoneParticleData.REDSTONE_DUST;
-		worldIn.addParticle(data, d0, d1, d2, 0, 1, 0);
+		Direction facing = stateIn.get(BlockStateProperties.HORIZONTAL_FACING).getOpposite();
+
+		d0 = d0 + 0.27D * (double) facing.getXOffset();
+		d1 = d1 + 0.22D;
+		d2 = d2 + 0.27D * (double) facing.getYOffset();
+
+		worldIn.addParticle(RedstoneParticleData.REDSTONE_DUST, d0, d1, d2, 0, 1, 0);
 	}
 }
