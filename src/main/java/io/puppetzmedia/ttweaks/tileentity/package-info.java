040ee232ff3bfa92bd2035934babc4470c7d4747
Matthew Cain <yooksi@zoho.com>
2020-05-01 01:21:37 +0200

    Annotate returns and params as nonnull

---
+++ src/main/java/io/puppetzmedia/ttweaks/tileentity/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package io.puppetzmedia.ttweaks.tileentity;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
