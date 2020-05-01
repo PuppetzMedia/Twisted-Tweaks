0c30068df77f2dfe0cf31f79feffbf9b306500ce
Matthew Cain <yooksi@zoho.com>
2020-05-01 08:32:48 +0200

    Fix fluid tank throwing NPE

--- src/main/java/io/puppetzmedia/ttweaks/tileentity/FluidTankTileEntity.java
+++ src/main/java/io/puppetzmedia/ttweaks/tileentity/FluidTankTileEntity.java
@@ -1,5 +1,6 @@
 package io.puppetzmedia.ttweaks.tileentity;
 
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.templates.FluidTank;
 
@@ -13,12 +14,22 @@ public class FluidTankTileEntity extends FluidTank {
 
 	@Override
 	public void setFluid(@Nullable FluidStack fluid) {
-		this.fluid = null;
+		// Fluid should always be empty
 	}
 
 	@Override
 	public int getFluidAmount() {
 		return 0;
+	}
+
+	@Override
+	public FluidTank readFromNBT(CompoundNBT nbt) {
+		return this;
+	}
+
+	@Override
+	public CompoundNBT writeToNBT(CompoundNBT nbt) {
+		return nbt;
 	}
 
 	@Override
