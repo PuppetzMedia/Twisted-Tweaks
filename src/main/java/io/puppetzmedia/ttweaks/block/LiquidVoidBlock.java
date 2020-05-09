06286fb80930a77005db2902620b7467e4661375
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-05-09 16:04:52 -0400

    add mekanism for testing and fix fluidvoid

--- src/main/java/io/puppetzmedia/ttweaks/block/LiquidVoidBlock.java
+++ src/main/java/io/puppetzmedia/ttweaks/block/LiquidVoidBlock.java
@@ -3,9 +3,20 @@ package io.puppetzmedia.ttweaks.block;
 import io.puppetzmedia.ttweaks.tileentity.LiquidVoidTileEntity;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fluids.FluidActionResult;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.items.wrapper.PlayerInvWrapper;
 
 public class LiquidVoidBlock extends Block {
 
@@ -14,8 +25,20 @@ public class LiquidVoidBlock extends Block {
     }
 
     @Override
-    public BlockRenderType getRenderType(BlockState state) {
-        return BlockRenderType.MODEL;
+    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult result) {
+        ItemStack itemHeld = player.getHeldItem(hand);
+        TileEntity tank = world.getTileEntity(pos);
+        final boolean[] success = {false};
+            itemHeld.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY).ifPresent(handler -> {
+                PlayerInvWrapper invWrapper = new PlayerInvWrapper(player.inventory);
+                FluidActionResult fillResult = FluidUtil.tryEmptyContainerAndStow(itemHeld,
+                        tank.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY).orElse(null), invWrapper, Integer.MAX_VALUE, player, true);
+                if (fillResult.isSuccess()) {
+                    player.setHeldItem(hand, fillResult.getResult());
+                    success[0] = true;
+                }
+            });
+        return success[0] ? ActionResultType.SUCCESS : ActionResultType.PASS;
     }
 
     @Override
--- src/main/java/io/puppetzmedia/ttweaks/tileentity/LiquidVoidTileEntity.java
+++ src/main/java/io/puppetzmedia/ttweaks/tileentity/LiquidVoidTileEntity.java
@@ -1,9 +1,19 @@
 package io.puppetzmedia.ttweaks.tileentity;
 
 import io.puppetzmedia.ttweaks.TwistedTweaks;
+import io.puppetzmedia.ttweaks.inventory.VoidFluidTank;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.TileFluidHandler;
 import net.minecraftforge.registries.ObjectHolder;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 @ObjectHolder(TwistedTweaks.MODID)
 public class LiquidVoidTileEntity extends TileFluidHandler {
@@ -11,11 +21,20 @@ public class LiquidVoidTileEntity extends TileFluidHandler {
 	@ObjectHolder("liquid_void_te")
 	public static final TileEntityType<LiquidVoidTileEntity> ENTITY_TYPE = null;
 
-	public static final int CAPACITY = Integer.MAX_VALUE;
-
 	public LiquidVoidTileEntity() {
-
 		super(ENTITY_TYPE);
-		tank = new FluidTankTileEntity(CAPACITY);
 	}
+
+	protected VoidFluidTank tank = new VoidFluidTank();
+
+	private final LazyOptional<IFluidHandler> holder = LazyOptional.of(() -> tank);
+
+	@Override
+	@Nonnull
+	public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction facing) {
+		if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)
+			return holder.cast();
+		return super.getCapability(capability, facing);
+	}
+
 }
